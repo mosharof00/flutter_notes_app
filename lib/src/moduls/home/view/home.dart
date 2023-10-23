a1928121e20697c70b4f5685ad98a2124e1ab9f6
Mosharof Khan <mosharof5khan@gmail.com>
2023-10-24 01:31:03 +0600

    add deleteNote

--- lib/src/moduls/home/view/home.dart
+++ lib/src/moduls/home/view/home.dart
@@ -1,4 +1,6 @@
+import 'dart:js_interop';
 import 'dart:math';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:notes/src/data/constants/colors.dart';
@@ -7,6 +9,7 @@ import 'package:notes/src/moduls/add/view/add_task_screen.dart';
 import 'package:notes/src/moduls/edit/view/edit.dart';
 
 import '../../../data/models/note_model.dart';
+// import 'package:note_app/models/note.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,12 +19,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   List<Note> data = [];
   List filteredData = [];
   DBHelper dbHelper = DBHelper();
+  bool sorted = false;
 
-  Color getRandomColor(){
+  get filteredNotes => null;
+
+  Color getRandomColor() {
     Random random = Random();
     return backgroundColors[random.nextInt(backgroundColors.length)];
   }
@@ -33,7 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
   }
 
-  void getData()async {
+  void getData() async {
     data = await dbHelper.getCartData();
     // filteredData = data ;
     print(data.length);
@@ -53,6 +58,11 @@ class _HomeScreenState extends State<HomeScreen> {
   //         .where((text) => text.title!.toLowerCase().contains(value.toLowerCase())).toList();
   //   });}
 
+  void deleteNote(int index) {
+    setState(() {
+      filteredNotes.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -110,83 +120,86 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Expanded(
                 child: FutureBuilder(
-                  future: dbHelper.getCartData(),
-                  builder: (context, snapshot) {
-                    return ListView.builder(
-                      padding: const EdgeInsets.only(top: 30),
-                      itemCount: data.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          //color: getRandomColor(),
-                          margin: const EdgeInsets.only(bottom: 20),
-                          elevation: 3,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: ListTile(
-                              title: InkWell(
-                                onTap: () {
-                                  Navigator.push(context, MaterialPageRoute(
-                                      builder: (context) => EditScreen()));
-                                },
-                                child: RichText(
-                                    maxLines: 3,
-                                    overflow: TextOverflow.ellipsis,
-                                    text: TextSpan(
-                                        text: "${data[index].title}\n",
-                                        style: const TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                            height: 1.8),
-                                        children: [
-                                          TextSpan(
-                                              text: data[index].subtitle,
-                                              style: const TextStyle(
-                                                  color: Colors.black,
-                                                  fontSize: 14,
-                                                  fontWeight: FontWeight.normal,
-                                                  height: 1.5
-                                              )
-                                          )
-                                        ]
-                                    )),
-                              ),
-                              subtitle: Padding(
-                                padding: const EdgeInsets.only(top: 8),
-                                child: Text(
-                                  'Edited: ${DateFormat.yMEd().add_jms().format(DateTime.parse(data[index].date))}',
-                                  style: const TextStyle(
-                                      fontSize: 11,
-                                      fontStyle: FontStyle.italic,
-                                      color: Colors.grey
+                    future: dbHelper.getCartData(),
+                    builder: (context, snapshot) {
+                      return ListView.builder(
+                        padding: const EdgeInsets.only(top: 30),
+                        itemCount: data.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            //color: getRandomColor(),
+                            margin: const EdgeInsets.only(bottom: 20),
+                            elevation: 3,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: ListTile(
+                                title: InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const EditScreen()));
+                                  },
+                                  child: RichText(
+                                      maxLines: 3,
+                                      overflow: TextOverflow.ellipsis,
+                                      text: TextSpan(
+                                          text: "${data[index].title}\n",
+                                          style: const TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              height: 1.8),
+                                          children: [
+                                            TextSpan(
+                                                text: data[index].subtitle,
+                                                style: const TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 14,
+                                                    fontWeight:
+                                                        FontWeight.normal,
+                                                    height: 1.5))
+                                          ])),
+                                ),
+                                subtitle: Padding(
+                                  padding: const EdgeInsets.only(top: 8),
+                                  child: Text(
+                                    'Edited: ${DateFormat.yMEd().add_jms().format(DateTime.parse(data[index].date))}',
+                                    style: const TextStyle(
+                                        fontSize: 11,
+                                        fontStyle: FontStyle.italic,
+                                        color: Colors.grey),
                                   ),
                                 ),
-                              ),
-                              trailing: IconButton(
-                                onPressed: ()  {
-                                  setState((){
+                                trailing: IconButton(
+                                  onPressed: () async {
+                                    final result = await confirmDialog(context);
+                                    if (result != null && result) {
+                                      deleteNote(index);
+                                    }
+                                    setState(() {
                                       dbHelper.delete(data[index].id);
-                                    debugPrint(data.length.toString());
-                                  });
-                                },
-                                icon: const Icon(Icons.delete),
+                                      debugPrint(data.length.toString());
+                                    });
+                                  },
+                                  icon: const Icon(Icons.delete),
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      },
-                    );
-
-                  }
-                )),
+                          );
+                        },
+                      );
+                    })),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>AddTaskScreen()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const AddTaskScreen()));
         },
         elevation: 10,
         backgroundColor: Colors.grey.shade800,
@@ -196,5 +209,56 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+
+  Future<dynamic> confirmDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.grey.shade900,
+            icon: const Icon(
+              Icons.info,
+              color: Colors.grey,
+            ),
+            title: const Text(
+              "Are you sure you want to delete?",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white),
+            ),
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  },
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  child: const SizedBox(
+                      width: 60,
+                      child: Text(
+                        "Yes",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                  },
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  child: const SizedBox(
+                      width: 60,
+                      child: Text(
+                        "no",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      )),
+                )
+              ],
+            ),
+          );
+        });
   }
 }
